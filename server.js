9d32d1c9736b0ebb8542a444c262dbcba0360e06
Rob Cameron <rwcameron@gmail.com>
2012-08-02 15:53:05 -0700

    moved insert to emit model

--- server.js
+++ server.js
@@ -1,69 +1,58 @@
 var fs = require('fs');
-var mongo = require('mongodb');
+var util = require('util');
+var events = require('events');
+//
+var mongodb = require('mongodb');
+var lazy = require("lazy");
+//
 var geoArray = new Array();
-var recordCount = 1;
+var server = new mongodb.Server("127.0.0.1", 27017, {});
+
+var db = new mongodb.Db('geo', server, {});
+
+var GeoParser = function(filename) {
+  events.EventEmitter.call(this);
+  this.filename = filename;
+};
+
+util.inherits(GeoParser, events.EventEmitter);
+
+GeoParser.prototype.parseFile = function() {
+  var self = this;
+  try {
+    new lazy(fs.createReadStream(self.filename)).lines.forEach(function(line) {
+      var splitLine = line.toString().split(',');
+      if (splitLine instanceof Array && !! splitLine[9]) {
+        self.emit('record', new GeoCityEntry(splitLine[0].replace(/\"/g, ''), splitLine[1].replace(/\"/g, ''), splitLine[2].replace(/\"/g, ''), splitLine[3].replace(/\"/g, ''), splitLine[4].replace(/\"/g, ''), splitLine[5].replace(/\"/g, ''), splitLine[6].replace(/\"/g, ''), splitLine[7].replace(/\"/g, ''), splitLine[8].replace(/\"/g, ''), splitLine[9].replace(/\"/g, '')))
+      };
+    });
+  } catch (error) {
+    console.log('Unable to open file');
+  };
+};
 
 var GeoEntry = function(subnetStart, subnetEnd, numberStart, numberEnd, countryCode, countryString) {
   this.subnetStart = subnetStart, this.subnetEnd = subnetEnd, this.numberStart = parseInt(numberStart), this.numberEnd = parseInt(numberEnd), this.countryCode = countryCode, this.countryString = countryString
 };
 
-GeoEntry.constructor = GeoEntry;
-find({
-  $and: [{
-    'numberStart': {
-      $gte: 134744064
-    }
-  }, {
-    'numberEnd': {
-      $lte: 134744064
-    }
-  }]
-})
-find( { $and: [ { a: 1 }, { a: { $gt: 5 } } ] } )
+var GeoCityEntry = function(startIpNum, endIpNum, country, region, city, postalCode, latitude, longitude, dmaCode, areaCode) {
+  this.startIp = startIpNum, this.startIpNum = ipToNum(startIpNum), this.endIpNum = ipToNum(endIpNum), this.endIp = endIpNum, this.country = country, this.region = region, this.city = city, this.postalCode = postalCode, this.latitude = latitude, this.longitude = longitude, this.dmaCode = dmaCode, this.areaCode = areaCode
+};
 
-var pushToMongo = function(array) {
-  var db = new mongo.Db('geo', new mongo.Server('localhost', 27017, {}), {});
-  db.open(function(err, result) {
-    db.dropCollection('iptocn', function(err, result) {
-      db.collection('iptocn', function(err, collection) {
-        for (item in array) {
-          collection.insert(array[item], function(err, docs) {
-            if (err) {
-              console.log(err);
-            } else {
-              //continue
-              process.stdout.write(".");
-              recordCount++;
-            };
-          });
-        };
-        console.log('');
-        console.log('Insert complete - Total Records: ' + recordCount);
-        db.close();
-        return 0;
+var ipToNum = function(ip) {
+  var ipArray = ip.split('.');
+  return (16777216 * ipArray[0]) + (65536 * ipArray[1]) + (256 * ipArray[2]) + (ipArray[3]);
+}
+
+var geoParse = new GeoParser(process.argv[2]);
+geoParse.parseFile();
+
+db.open(function(err, result) {
+  db.dropCollection('iptocncity', function(err, result) {
+    geoParse.on('record', function(data) {
+      db.collection('iptocncity', function(err, collection) {
+        collection.insert(data);
       });
     });
   });
-};
-
-console.log('');
-process.stdout.write('Starting File Read...');
-var data = fs.readFileSync('./GeoIPCountryWhois.csv', 'utf8');
-process.stdout.write("Complete!");
-
-console.log('');
-process.stdout.write('Starting File Parse...');
-var fileArray = data.split('\n');
-for (line in fileArray) {
-  process.stdout.write(".");
-  var splitLine = fileArray[line].split(',');
-  if (splitLine instanceof Array && !! splitLine[5]) {
-    var geoEnt = new GeoEntry(splitLine[0].replace(/\"/g, ''), splitLine[1].replace(/\"/g, ''), splitLine[2].replace(/\"/g, ''), splitLine[3].replace(/\"/g, ''), splitLine[4].replace(/\"/g, ''), splitLine[5].replace(/\"/g, ''));
-    geoArray.push(geoEnt);
-  };
-};
-process.stdout.write("Complete!");
-
-console.log('');
-console.log('Starting Insert...')
-pushToMongo(geoArray);
+});
